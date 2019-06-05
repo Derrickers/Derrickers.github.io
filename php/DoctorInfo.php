187fafec357eff7b2e3b18dc1154b737422daa18
SoakedThrough <32643629+SoakedThrough@users.noreply.github.com>
2019-06-05 16:27:04 +0800

    Update DoctorInfo.php

--- php/DoctorInfo.php
+++ php/DoctorInfo.php
@@ -40,21 +40,14 @@ else {
   exit;
 }
 
-$T = "<table border='1'><tr><th>url</th><th>id</th><th>hospital</th><th>name</th><th>department</th></tr>";
-
-html_entity_decode($T,ENT_QUOTES, 'UTF-8');
-
-echo $T;
-
-while($row = mysqli_fetch_array($result))
-{
-  echo "<tr>";
-  echo "<td>" . $row['link'] . "</td>";
-  echo "<td>" . $row['doc_id'] . "</td>";
-  echo "<td>" . $row['hos_name'] . "</td>";
-  echo "<td>" . $row['doc_name'] . "</td>";
-  echo "<td>" . $row['department'] . "</td>";
-  echo "</tr>";
+$result = mysqli_query($con,$sql);
+if($result){
+  echo "<br><br><hr><br><table border='1'><tr><th>url</th><th>id</th><th>hospital</th><th>name</th><th>faculty</th></tr>";
+  while($row = mysqli_fetch_array($result))
+  {
+      echo "<br><br><hr><br><table border='1'><tr><th>$row['link']</th><th>$row['doc_id']</th><th>$row['hos_name']</th><th>$row['doc_name']</th><th>$row['department']</th></tr>";
+  }
+  echo "</table><br><hr><br><br><br>";
 }
-echo "</table>";
+mysqli_close($con);
 ?>
