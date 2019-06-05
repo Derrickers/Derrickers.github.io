da7463e8271fa6025cd0775faf9d4331e7984af8
SoakedThrough <32643629+SoakedThrough@users.noreply.github.com>
2019-06-05 20:48:25 +0800

    Update DoctorInfo.php

--- php/DoctorInfo.php
+++ php/DoctorInfo.php
@@ -2,52 +2,7 @@
 header("Content-Type:text/html;charset=utf-8");
 $q = isset($_GET["q"]) ? intval($_GET["q"]) : '';
 
-/*if(empty($q)) {
-    echo "请输入检索条件";
-    exit;
-}
 
-//$con = mysqli_connect('10.203.209.240','root','25981745');
-$con = new MySQLi("localhost","root","25981745","data");
-if (!$con)
-{
-    die('Could not connect: ' . mysqli_error($con));
-}else {
-  echo "aaaaafffffff";
-}
-
-//mysqli_select_db($con,"data");
-
-//mysqli_set_charset($con, "utf8");
-
-$tok = strtok($q," ");
-if ($tok == "1") {
-  $tok = strtok(" ");
-  $sql="SELECT * FROM doc_info WHERE hos_name = '".$tok."'";
-}elseif ($tok == "2") {
-  $tok = strtok(" ");
-  $sql="SELECT * FROM doc_info WHERE doc_name = '".$tok."'";
-}elseif ($tok == "3") {
-  $tok = strtok(" ");
-  $sql="SELECT * FROM doc_info WHERE doc_id = '".$tok."'";
-}elseif ($tok == "4") {
-  $tok = strtok(" ");
-  $sql="SELECT * FROM doc_info WHERE department = '".$tok."'";
-}else {
-  echo "请输入检索条件";
-  exit;
-}
-
-$result = mysqli_query($con,$sql);
-if($result){
-  echo "<br><br><br><table border='1'><tr><th>url</th><th>id</th><th>hospital</th><th>name</th><th>faculty</th></tr>"；
-  while($row = mysqli_fetch_array($result))
-  {
-  echo "<br><br><br><table border='1'><tr><th> $row['link'] </th><th> $row['doc_id'] </th><th> $row['hos_name'] </th><th> $row['doc_name'] </th><th> $row['department'] </th></tr>";
-  }
-  echo "</table><br><br><br><br> ";
-}
-mysqli_close($con);*/
 echo <<<EOF
 <table border='1'><tr><th>url</th><th>id</th><th>hospital</th><th>name</th><th>department</th></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>孔繁智</td><td>呼吸内科</td></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>李国平</td><td>呼吸内科</td></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>姜琴华</td><td>呼吸内科</td></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>柴秀娟</td><td>呼吸内科</td></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>陈明</td><td>呼吸内科</td></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>周颖</td><td>呼吸内科</td></tr><tr><td>NULL</td><td>NULL</td><td>浙江省立同德医院</td><td>徐卫华</td><td>呼吸内科</td></tr></table><br><br><br>
 EOF;
